13982ed5d3e945add38e901177f3ed5d197b14b4
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-09-13 13:06:51 +0200

    Publish library to Maven Central. (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
 plugins {
-    id("java")
+    id("java-library")
     checkstyle
     id("com.diffplug.spotless") version "6.25.0"
+
+    id("maven-publish")
+    signing
+    id("tech.yanand.maven-central-publish") version "1.2.0"
 }
 
 group = "io.github.jeschkies"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -20,6 +27,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers:1.20.1")
 }
 
 checkstyle {
@@ -34,4 +42,77 @@ spotless {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            version = "0.0.1"
+
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name = "Loki Client"
+                description = "Loki Java client that sends and retrieves logs to and from a running Loki server"
+                url = "https://www.github.com/jeschkies/loki-client-java"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                signing {
+                    sign(publishing.publications["mavenJava"])
+                    sign(configurations["archives"])
+                }
+                developers {
+                    developer {
+                        id = "jeschkies"
+                        name = "Karsten Jeschkies"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com:jeschkies/loki-client-java.git"
+                    developerConnection= "scm:git:ssh://github.com:jeschkies/loki-client-java.git"
+                    url = "https://github.com/jeschkies/loki-client-java"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Local"
+            url = uri(layout.buildDirectory.dir("repos/bundles"))
+        }
+    }
+}
+
+signing {
+    val keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+    val secretKey = System.getenv("SIGNING_KEY")
+    useInMemoryPgpKeys(secretKey, keyPassword)
+    sign(publishing.publications["mavenJava"])
+}
+
+mavenCentral {
+    repoDir = layout.buildDirectory.dir("repos/bundles")
+    val user = System.getenv("MAVEN_PORTAL_USER")
+    val password = System.getenv("MAVEN_PORTAL_PASSWORD")
+    @OptIn(ExperimentalEncodingApi::class)
+    authToken = Base64.Default.encode("$user:$password".encodeToByteArray())
+    publishingType = "AUTOMATIC"
 }
--- src/test/java/io/github/jeschkies/loki/client/TestIntegration.java
+++ src/test/java/io/github/jeschkies/loki/client/TestIntegration.java
@@ -1,5 +1,48 @@
 package io.github.jeschkies.loki.client;
 
-public class TestIntegration {
-  private LokiClient client;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import io.github.jeschkies.loki.LokiTestServer;
+import io.github.jeschkies.loki.client.model.Data;
+import io.github.jeschkies.loki.client.model.QueryResult;
+import io.github.jeschkies.loki.client.model.Streams;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestIntegration {
+  private static LokiClient client;
+  private static LokiTestServer server;
+
+  // TODO: maybe make this a resource
+  @BeforeAll
+  public static void setup() throws IOException {
+    server = new LokiTestServer();
+    client = new LokiClient(new LokiClientConfig(server.getUri(), Duration.ofSeconds(10)));
+  }
+
+  @Test
+  void TestRoundTrip() throws IOException, LokiClientException {
+    Instant start = Instant.now().minus(Duration.ofHours(3));
+    Instant end = start.plus(Duration.ofHours(2));
+
+    client.pushLogLine(
+        "line foo", end.minus(Duration.ofMinutes(5)), ImmutableMap.of("test", "roundtrip"));
+    client.flush();
+    QueryResult result = client.rangeQuery("{test=\"roundtrip\"}", start, end);
+    assertThat(result.getData().getResultType()).isEqualTo(Data.ResultType.Streams);
+    assertThat(result.getData().getResult()).isInstanceOf(Streams.class);
+    var streams = ((Streams) result.getData().getResult()).getStreams();
+    assertThat(streams).hasSize(1);
+    assertThat(streams.getFirst().values().getFirst().getLine()).isEqualTo("line foo");
+  }
+
+  @AfterAll
+  public static void teardown() {
+    server.close();
+  }
 }
