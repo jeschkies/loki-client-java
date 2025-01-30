ffaa66ef91aaeb674f4fd188df104610d644cf50
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2025-01-30 18:22:39 +0100

    Accept step length as parameter. (#9)

--- loki-client/build.gradle.kts
+++ loki-client/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "io.github.jeschkies"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
     mavenCentral()
@@ -54,7 +54,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "io.github.jeschkies"
             artifactId = "loki-client"
-            version = "0.0.2"
+            version = "0.0.3"
 
             from(components["java"])
 
--- loki-client/src/test/java/io/github/jeschkies/loki/client/TestIntegration.java
+++ loki-client/src/test/java/io/github/jeschkies/loki/client/TestIntegration.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableMap;
 import io.github.jeschkies.loki.LokiTestServer;
 import io.github.jeschkies.loki.client.model.Data;
+import io.github.jeschkies.loki.client.model.Matrix;
 import io.github.jeschkies.loki.client.model.QueryResult;
 import io.github.jeschkies.loki.client.model.Streams;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -27,11 +29,11 @@ class TestIntegration {
 
   @Test
   void TestRoundTrip() throws IOException, LokiClientException {
-    Instant start = Instant.now().minus(Duration.ofHours(3));
+    Instant start = Instant.now().truncatedTo(ChronoUnit.HOURS).minus(Duration.ofHours(3));
     Instant end = start.plus(Duration.ofHours(2));
 
     client.pushLogLine(
-        "line foo", end.minus(Duration.ofMinutes(5)), ImmutableMap.of("test", "roundtrip"));
+        "line foo", start.plus(Duration.ofMinutes(4)), ImmutableMap.of("test", "roundtrip"));
     client.flush();
     QueryResult result = client.rangeQuery("{test=\"roundtrip\"}", start, end);
     assertThat(result.getData().getResultType()).isEqualTo(Data.ResultType.Streams);
@@ -39,6 +41,17 @@ class TestIntegration {
     var streams = ((Streams) result.getData().getResult()).getStreams();
     assertThat(streams).hasSize(1);
     assertThat(streams.getFirst().values().getFirst().getLine()).isEqualTo("line foo");
+
+    result = client.rangeQuery("count_over_time({test=\"roundtrip\"}[5m])", start, end, 300);
+    assertThat(result.getData().getResultType()).isEqualTo(Data.ResultType.Matrix);
+    assertThat(result.getData().getResult()).isInstanceOf(Matrix.class);
+    var metrics = ((Matrix) result.getData().getResult()).getMetrics();
+    assertThat(metrics).hasSize(1);
+    var values = metrics.getFirst().values();
+    assertThat(values).hasSize(1);
+    assertThat(values.getFirst().getValue()).isEqualTo(1.0);
+    assertThat(Instant.ofEpochSecond(values.getFirst().getTs()))
+        .isEqualTo(start.plus(Duration.ofMinutes(5)));
   }
 
   @AfterAll
